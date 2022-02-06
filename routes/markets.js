4baaf82dfe0c9a47bb8f5b2f96bbf4aea43780ea
BonhaengLee <bon952014@ajou.ac.kr>
2022-02-07 03:17:35 +0900

    [Feat] convert DATE for markets

--- routes/markets.js
+++ routes/markets.js
@@ -1,7 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
+const { investing } = require("investing-com-api");
+
+async function main() {
+  try {
+    const response1 = await investing("currencies/eur-usd");
+    // const response2 = await investing("currencies/eur-usd", 3600, 24, "1-day"); // With optional params
+    // const response3 = await investing("currencies/eur-usd", 1800, 24, "1-hour"); // With optional params
+    // const response4 = await investing(
+    //   "currencies/eur-usd",
+    //   900,
+    //   24,
+    //   "1-minute"
+    // );
+    console.log(
+      "1",
+      response1.map((item) => ({
+        time: Date(item.time),
+        value: item.value,
+      }))
+    );
+    // console.log("2", response2);
+    // console.log("3", response3);
+    // console.log("4", response4);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 router.get("/", (req, res) => {
+  main();
   res.send("Hello world");
 });
 
